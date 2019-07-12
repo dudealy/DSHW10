da515218ed9bb543fabcc39f64976b0362bd1bbf
dudealy <36426459+dudealy@users.noreply.github.com>
2019-07-11 23:42:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -41,10 +41,10 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>" 
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>" 
     )
 
 
@@ -55,16 +55,21 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).all()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    dates_prcp = []
+    for date, precipitation in results:
+        dates_prcp_dict={}
+        dates_prcp_dict['date']= date
+        dates_prcp_dict['pricipitation']=prcp
+        dates_prcp.append(dates_prcp_dict)
 
-    return jsonify(all_names)
+    return jsonify(dates_prcp)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
     """Return a list of all stations"""
     # Query all stations
-    results = session.query(Measurement.station).all()
+    results = session.query(Station.station).all()
     all_stations=list(np.ravel(results))
     return jsonify(all_stations)
 
@@ -72,8 +77,28 @@ def stations():
 def tobs():
     """Return a list of dates and temperature observations"""
     # Query dates and temperature observations from a year from the last data point
-    results = session.query(Measurement.station).all()    
-    return jsonify(results)
+    query = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= dt.date(2017,8,23)-dt.timedelta(days=365)).all()    
+    last_year_tobs=list(np.ravel(query))
+    return jsonify(last_year_tobs)
+
+@app.route("/api/v1.0/start")
+def start_date():
+    """Return a list of total min, avg, and max for all dates equal to and greater than the start date"""
+    # Create function
+    def calc_temps(start_date):
+        query=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date>=start_date).all()
+        start_date_list=list(np.ravel(query))
+        return jsonify(start_date_list)
+
+
+@app.route("/api/v1.0/start/end")
+def end_date():
+    """Return a list of total min, avg, and max for all dates between the start and end date"""
+    # Create function
+    def calc_temps(start_date):
+        query=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        start_end_list=list(np.ravel(query))
+        return jsonify(start_end_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
