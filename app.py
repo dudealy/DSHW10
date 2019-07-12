1f9a28569094c2b4450291be7f3f4e7916e349f6
dudealy <36426459+dudealy@users.noreply.github.com>
2019-07-11 22:34:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
+ 
 # Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
@@ -28,7 +28,7 @@ session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__climate__)
+app = Flask(__name__)
 
 
 #################################################
@@ -40,16 +40,19 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/p<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>" 
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return a dictionary of all dates and their precipitation value"""
+    # Query 
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
     # Convert list of tuples into normal list
     all_names = list(np.ravel(results))
@@ -57,23 +60,20 @@ def names():
     return jsonify(all_names)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(Measurement.station).all()
+    all_stations=list(np.ravel(results))
+    return jsonify(all_stations)
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
-
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of dates and temperature observations"""
+    # Query dates and temperature observations from a year from the last data point
+    results = session.query(Measurement.station).all()    
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
